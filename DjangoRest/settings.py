6ef9ef16eea28623e8f02ae6cf5d10408eb2268c
VISHAL SHAKYA <shakayavishal007@gmail.com>
2020-07-28 18:17:01 +0530

    after confingrations

--- DjangoRest/settings.py
+++ DjangoRest/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 temp_dir = os.path.join(BASE_DIR,'build')
-static_dir = os.path.join(BASE_DIR,'build/static/')
+static_dir = os.path.join(BASE_DIR,'build/static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
